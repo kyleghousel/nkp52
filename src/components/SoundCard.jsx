c91a1e4a783e3b4ca0306069f52a0c9e8f888eb4
Kyle Housel <kylegranthousel@gmail.com>
2025-06-30 11:47:38 -0400

    Play associated audio onClick

--- src/components/SoundCard.jsx
+++ src/components/SoundCard.jsx
@@ -4,8 +4,14 @@ const SoundCard = ({ sound }) => {
 
   const { audio, id, name, image } = sound
 
+  const playSound =  () => {
+    const soundPath = `${process.env.PUBLIC_URL}${audio}`;
+    const audioPlayer = new Audio(soundPath);
+    audioPlayer.play();
+  }
+
   return (
-    <div id={id} className="card h-100 soundCard">
+    <div id={id} className="card h-100 soundCard" onClick={playSound}>
       <img src={image} alt={name} />
       <h4 className="card-body text-center">{name}</h4>
     </div>
